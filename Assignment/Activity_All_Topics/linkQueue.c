98e6dc7c201911a34c17741e4ab24535eb10b2ea
NemesisTon <tonflores99@gmail.com>
2025-09-15 19:11:07 +0800

    Link queue added (unfinished)

--- Assignment/Activity_All_Topics/linkQueue.c
+++ Assignment/Activity_All_Topics/linkQueue.c
@@ -17,70 +17,67 @@ typedef struct Queue{
 }Queue;
 
 bool isFull(Queue* q){
-    return q->front == (q->rear + 2) % MAX;
+    return false;
 }
 
 bool isEmpty(Queue* q){
-    return q->front == (q->rear + 1) % MAX;
+    return q->front == NULL;
 }
 
 int front(Queue* q){
-    int get = 0;
-    if(isEmpty(q)){
-        printf("The queue is empty.\n");
-    }
-    get = q->item[q->front];
-    return isEmpty(q) ? -1 : get;
+    // int get = 0;
+    // if(isEmpty(q)){
+    //     printf("The queue is empty.\n");
+    // }
+    // get = q->item[q->front];
+    // return isEmpty(q) ? -1 : get;
 }
 
 int dequeue(Queue* q){
-    int get = 0;
-    if(isEmpty(q)){
-        printf("The queue is empty.\n");
-    }else{
-        get = q->item[q->front];
-        q->front = (q->front + 1) % MAX;
-    }
-    return isEmpty(q) ? -1 : get;
+    // int get = 0;
+    // if(isEmpty(q)){
+    //     printf("The queue is empty.\n");
+    // }else{
+    //     get = q->item[q->front];
+    //     q->front = (q->front + 1) % MAX;
+    // }
+    // return isEmpty(q) ? -1 : get;
 }
 
 void enqueue(Queue* q, int value){
-    if(isFull(q)){
-        printf("The queue is empty.\n");
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = value;
+    if(isEmpty(q)){
+        q->front = newNode;
+        newNode->next = NULL;
     }else{
-        q->rear = (q->rear + 1) % MAX;
-        q->item[q->rear] = value;
+        Node *temp = q->front;
+        for(; temp->next != NULL; temp = temp->next){}
+        temp->next = newNode;
+        newNode->next = NULL;
+        q->rear = temp;
     }
 }
 
 void display(Queue* q){
-    // if (isEmpty(q)) {
-    //     printf("Queue is empty.\n\n");
-    // }else{
-    //     char buffer[256] = "Elem: [";
-    //     char temp[20];
+    if (isEmpty(q)) {
+        printf("The queue is empty.\n\n");
+    }else{
+        char buffer[256] = "Elem: Front --> ";
+        char temp1[20];
 
-    //     int count;
-    //     if (q->rear >= q->front) {
-    //         count = q->rear - q->front + 1;
-    //     } else {
-    //         count = (MAX - q->front) + (q->rear + 1);
-    //     }
-
-    //     int i = q->front, j = 0;
-    //     while (j < count) {
-    //         if (j < count - 1) {
-    //             sprintf(temp, "%d, ", q->item[i]);
-    //         } else {
-    //             sprintf(temp, "%d]", q->item[i]);
-    //         }
-    //         strcat(buffer, temp);
-    //         i = (i + 1) % MAX;
-    //         j++;
-    //     }
-
-    //     printf("%-50s Count: %d\tFront: %d\tRear: %d\n\n", buffer, count, q->front, q->rear);
-    // }
+        Node *temp = q->front;
+        int i = 0;
+        for(; temp != NULL; temp = temp->next, i++){
+            if(temp->next != NULL){
+                sprintf(temp1, "%d --> ", temp->data);
+            }else{
+                sprintf(temp1, "%d --> Rear", temp->data);
+            }
+            strcat(buffer, temp1);
+        }
+        printf("%-75s Count: %d\n\n", buffer, i);
+    }
 }
 
 Queue* initialize(){
@@ -93,13 +90,13 @@ Queue* initialize(){
 int main(){
     Queue *q = initialize();
 
-    // printf("Queue Array\n");
-    // enqueue(q, 10);
-    // enqueue(q, 20);
-    // enqueue(q, 30);
-    // enqueue(q, 40);
-    // enqueue(q, 50);
-    // display(q);
+    printf("Queue Array\n");
+    enqueue(q, 10);
+    enqueue(q, 20);
+    enqueue(q, 30);
+    enqueue(q, 40);
+    enqueue(q, 50);
+    display(q);
 
     // printf("Dequeue Array\n");
     // int get = dequeue(q);
---
+++ Assignment/Activity_All_Topics/tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+    printf("Queue Array\n");
+    enqueue(q, 10);
+    enqueue(q, 20);
+    enqueue(q, 30);
+    enqueue(q, 40);
+    enqueue(q, 50);
